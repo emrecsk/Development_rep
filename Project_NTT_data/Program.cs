8fedea27ce0ec74573eefb9415363aa44323e2f5
Emre <emre.coskun@bs.nttdata.com>
2023-01-04 09:23:10 +0100

    Service Filter and README added

--- Project_NTT_data/Program.cs
+++ Project_NTT_data/Program.cs
@@ -46,7 +46,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseMiddleware<IPSafeMiddleWare>(); //Butun uygulama icin bunu ekledik. Eger bu listede olmayan bir IP adresinden istek yapilirsa uygulamaya erisilemeyecek. Reference line 21
+app.UseMiddleware<IPSafeMiddleWare>(); //Butun uygulama icin bunu ekledik. Eger bu listede olmayan bir IP adresinden istek yapilirsa uygulamaya erisilemeyecek. Reference line 22
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSession();
---
+++ README.md
@@ -0,0 +1,2 @@
+# NTT_data
+This is a development project. I am trying new features in this project to develop my code skills.
